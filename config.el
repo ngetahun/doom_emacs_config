07789bf95e4c3ba3a3dca3c0d0ace4609d87b338
Natnael Getahun <connect@ngetahun.me>
2022-08-29 13:24:27 +0200

    Initial commit

---
+++ config.el
@@ -0,0 +1,171 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets. It is optional.
+(setq user-full-name "Natnael Getahun"
+      user-mail-address "connect@ngetahun.me")
+
+
+; General
+
+;; Bindings for managing windows, buffers, etc.
+(map! "C-x k" #'kill-this-buffer)
+(map! "C-x K" #'kill-buffer-and-window)
+(map! "C-x v" #'split-window-right)
+(map! "C-x V" (lambda () (interactive) (split-window-right) (other-window 1)))
+
+;; OSX fix: Map meta key
+;;
+
+;(cond (IS-MAC
+;       (setq mac-command-modifier      'meta
+;             mac-option-modifier       'alt
+;             mac-right-option-modifier 'super)))
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom:
+;;
+;; - `doom-font' -- the primary font to use
+;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
+;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;; - `doom-unicode-font' -- for unicode glyphs
+;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
+;;
+;; See 'C-h v doom-font' for documentation and more examples of what they
+;; accept. For example:
+;;
+;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
+;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
+;;
+;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
+;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
+;; refresh your font settings. If Emacs still can't find your font, it likely
+;; wasn't installed correctly. Font issues are rarely Doom issues!
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-one)
+
+;; Some time ago I created this title format, and now I prefer it over other
+;; options, including Doom's default one.
+(setq frame-title-format
+      (setq icon-title-format '((:eval (concat (user-real-login-name) ": "
+                                               (if (buffer-file-name)
+                                                   (abbreviate-file-name (buffer-file-name))
+                                                 "%b"))))))
+(setq doom-font (font-spec :family "Hack" :size 14 :weight 'regular)
+      doom-variable-pitch-font (font-spec :family "Hack") ; inherits `doom-font''s :size
+      doom-unicode-font (font-spec :family "Hack" :size 12)
+      doom-big-font (font-spec :family "Hack" :size 19))
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type 'relative)
+
+;; The modeline I was using even before switching to Doom. I modify quite a few
+;; things from the default installation so it is simpler.
+(use-package! doom-modeline
+  :init
+  (setq doom-modeline-height 25
+        doom-modeline-persp-name nil
+        doom-modeline-major-mode-icon nil
+        doom-modeline-modal-icon t
+        doom-modeline-icon t
+        doom-modeline-buffer-file-name-style 'relative-from-project))
+
+; evil configs
+(map! :i "C-s" (lambda () (interactive) (save-buffer) (evil-force-normal-state)))
+
+;; Terminal configuration for OSX
+;;
+(use-package vterm
+  :ensure t
+  :when (memq window-system '(mac ns x pgtk))
+  :bind (:map vterm-mode-map
+              ("C-y" . vterm-yank)
+              ("M-y" . vterm-yank-pop)
+              ("C-k" . vterm-send-C-k-and-kill))
+:custom
+  (vterm-shell "bash")
+  (vterm-always-compile-module t)
+  :config
+  (defun vterm-send-C-k-and-kill ()
+    "Send `C-k' to libvterm, and put content in kill-ring."
+    (interactive)
+    (kill-ring-save (point) (vterm-end-of-line))
+    (vterm-send-key "k" nil nil t)))
+
+(use-package vterm-toggle
+  :ensure t
+  :when (memq window-system '(mac ns x pgtk))
+  :bind (([f8] . vterm-toggle)
+         ([f9] . vterm-compile)
+         :map vterm-mode-map
+         ([f8] . vterm-toggle)
+         ([(control return)] . vterm-toggle-insert-cd))
+  :custom
+  (vterm-toggle-cd-auto-create-buffer nil)
+  :config
+  (defvar vterm-compile-buffer nil)
+  (defun vterm-compile ()
+    "Compile the program including the current buffer in `vterm'."
+    (interactive)
+    (setq compile-command (compilation-read-command compile-command))
+    (let ((vterm-toggle-use-dedicated-buffer t)
+          (vterm-toggle--vterm-dedicated-buffer (if (vterm-toggle--get-window)
+                                                    (vterm-toggle-hide)
+                                                  vterm-compile-buffer)))
+      (with-current-buffer (vterm-toggle-cd)
+        (setq vterm-compile-buffer (current-buffer))
+        (rename-buffer "*vterm compilation*")
+        (compilation-shell-minor-mode 1)
+        (vterm-send-M-w)
+        (vterm-send-string compile-command t)
+        (vterm-send-return)))))
+
+;; Typescript config
+(require 'ansi-color)
+(defun colorize-compilation-buffer ()
+  (ansi-color-apply-on-region compilation-filter-start (point-max)))
+(add-hook 'compilation-filter-hook 'colorize-compilation-buffer)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/org/")
+
+
+;; Whenever you reconfigure a package, make sure to wrap your config in an
+;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
+;;
+;;   (after! PACKAGE
+;;     (setq x y))
+;;
+;; The exceptions to this rule:
+;;
+;;   - Setting file/directory variables (like `org-directory')
+;;   - Setting variables which explicitly tell you to set them before their
+;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
+;;   - Setting doom variables (which start with 'doom-' or '+').
+;;
+;; Here are some additional functions/macros that will help you configure Doom.
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
+;; etc).
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
